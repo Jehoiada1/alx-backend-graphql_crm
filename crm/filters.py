cc0396d162c040432424f3f4780a46b61d12a4e2
Yodahe Eshetu <yodaheeshetu880@gmail.com>
2025-09-27 22:16:03 +0300

    Tweak filter arg names to snake_case so GraphQL args use expected camelCase (e.g., createdAtGte, priceGte, totalAmountGte).

--- crm/filters.py
+++ crm/filters.py
@@ -7,8 +7,8 @@ from crm.orders.models import Order
 class CustomerFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
     email = filters.CharFilter(field_name="email", lookup_expr="icontains")
-    created_at__gte = filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
-    created_at__lte = filters.DateTimeFilter(field_name="created_at", lookup_expr="lte")
+    created_at_gte = filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
+    created_at_lte = filters.DateTimeFilter(field_name="created_at", lookup_expr="lte")
     phone_pattern = filters.CharFilter(method="filter_phone_pattern")
     order_by = filters.OrderingFilter(
         fields=(
@@ -25,15 +25,15 @@ class CustomerFilter(filters.FilterSet):
 
     class Meta:
         model = Customer
-        fields = ["name", "email", "created_at__gte", "created_at__lte", "phone_pattern"]
+    fields = ["name", "email", "created_at_gte", "created_at_lte", "phone_pattern"]
 
 
 class ProductFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
-    price__gte = filters.NumberFilter(field_name="price", lookup_expr="gte")
-    price__lte = filters.NumberFilter(field_name="price", lookup_expr="lte")
-    stock__gte = filters.NumberFilter(field_name="stock", lookup_expr="gte")
-    stock__lte = filters.NumberFilter(field_name="stock", lookup_expr="lte")
+    price_gte = filters.NumberFilter(field_name="price", lookup_expr="gte")
+    price_lte = filters.NumberFilter(field_name="price", lookup_expr="lte")
+    stock_gte = filters.NumberFilter(field_name="stock", lookup_expr="gte")
+    stock_lte = filters.NumberFilter(field_name="stock", lookup_expr="lte")
     order_by = filters.OrderingFilter(
         fields=(
             ("name", "name"),
@@ -44,14 +44,14 @@ class ProductFilter(filters.FilterSet):
 
     class Meta:
         model = Product
-        fields = ["name", "price__gte", "price__lte", "stock__gte", "stock__lte"]
+    fields = ["name", "price_gte", "price_lte", "stock_gte", "stock_lte"]
 
 
 class OrderFilter(filters.FilterSet):
-    total_amount__gte = filters.NumberFilter(field_name="total_amount", lookup_expr="gte")
-    total_amount__lte = filters.NumberFilter(field_name="total_amount", lookup_expr="lte")
-    order_date__gte = filters.DateTimeFilter(field_name="order_date", lookup_expr="gte")
-    order_date__lte = filters.DateTimeFilter(field_name="order_date", lookup_expr="lte")
+    total_amount_gte = filters.NumberFilter(field_name="total_amount", lookup_expr="gte")
+    total_amount_lte = filters.NumberFilter(field_name="total_amount", lookup_expr="lte")
+    order_date_gte = filters.DateTimeFilter(field_name="order_date", lookup_expr="gte")
+    order_date_lte = filters.DateTimeFilter(field_name="order_date", lookup_expr="lte")
     customer_name = filters.CharFilter(field_name="customer__name", lookup_expr="icontains")
     product_name = filters.CharFilter(field_name="products__name", lookup_expr="icontains")
     product_id = filters.NumberFilter(field_name="products__id")
@@ -66,10 +66,10 @@ class OrderFilter(filters.FilterSet):
     class Meta:
         model = Order
         fields = [
-            "total_amount__gte",
-            "total_amount__lte",
-            "order_date__gte",
-            "order_date__lte",
+            "total_amount_gte",
+            "total_amount_lte",
+            "order_date_gte",
+            "order_date_lte",
             "customer_name",
             "product_name",
             "product_id",
